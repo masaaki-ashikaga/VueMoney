e602683b2bf612b34b60bf5f376a945b45b139d4
masaaki-ashikaga <gamasaga0312@gmail.com>
2020-08-21 18:38:19 +0900

    firebase-configへ切り出し

---
+++ config/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA8v-UzUiYBpWMr7jFiFFPWisIV203O3rA",
+    authDomain: "vuejs-http-6b69d.firebaseapp.com",
+    databaseURL: "https://vuejs-http-6b69d.firebaseio.com",
+    projectId: "vuejs-http-6b69d",
+    storageBucket: "vuejs-http-6b69d.appspot.com",
+    messagingSenderId: "1092146060390",
+    appId: "1:1092146060390:web:768028660e5a7e2a79a1a7"
+  }
--- src/main.js
+++ src/main.js
@@ -4,18 +4,10 @@ import router from './router';
 import axios from 'axios';
 import store from './store';
 import firebase from 'firebase';
+import {firebaseConfig} from './../config/firebase-config';
 
 Vue.config.productionTip = false
 
-var firebaseConfig = {
-  apiKey: "AIzaSyA8v-UzUiYBpWMr7jFiFFPWisIV203O3rA",
-  authDomain: "vuejs-http-6b69d.firebaseapp.com",
-  databaseURL: "https://vuejs-http-6b69d.firebaseio.com",
-  projectId: "vuejs-http-6b69d",
-  storageBucket: "vuejs-http-6b69d.appspot.com",
-  messagingSenderId: "1092146060390",
-  appId: "1:1092146060390:web:768028660e5a7e2a79a1a7"
-};
 firebase.initializeApp(firebaseConfig);
 
 axios.defaults.baseURL = 'https://firestore.googleapis.com/v1/projects/vuejs-http-6b69d/databases/(default)/documents/';
